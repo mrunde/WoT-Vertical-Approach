0fa37aac0d7a6b9d4412c53114669df7507d1b05
avi92 <axelvirnich@googlemail.com>
2016-04-28 12:29:21 +0200

    Implemented things.listMeasurements API request

---
+++ server/www/server/controllers/things/listMeasurements.js
@@ -0,0 +1,49 @@
+// Required modules
+var mongoose = require('mongoose');
+
+// Required data schema
+var Sensor = require('../../data/sensor');
+
+/**
+ * @api {get} /things/:thingId/measurements GET - Request all Thing's measurements
+ * @apiName ListThingMeasurements
+ * @apiGroup Thing
+ * @apiVersion 1.0.0
+ *
+ * @apiParam {String} thingId	Thing's unique ID.
+ *
+ * @apiSuccess {Array} Measurements	Array of Measurements.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *       {
+ *         "_id": "<< generated MongoDB ID >>",
+ *         "sensorId": "<< generated MongoDB ID >>",
+ *         "date": "2016-04-24T16:56:45.000Z",
+ *         "value": 7,
+ *         "__v": 0
+ *       },
+ *       {
+ *         "_id": "<< generated MongoDB ID >>",
+ *         "sensorId": "<< generated MongoDB ID >>",
+ *         "date": "2016-04-24T16:59:45.000Z",
+ *         "value": 8,
+ *         "__v": 0
+ *       }
+ *     ]
+ *
+ * @apiUse ThingNotFoundError
+ * @apiUse ServerError
+ */
+exports.request = function(req, res) {
+	var id = req.params.thingId;
+
+	Measurement.find({ thingId: id }, function(err, measurements) {
+		if (err) {
+			res.send(err);
+		} else {
+			res.json(measurements);
+		}
+	});
+}
