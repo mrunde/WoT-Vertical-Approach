2736ebc5356d178114efb9a9da2788cde6666640
Marius Runde <marius.runde@uni-muenster.de>
2016-05-04 16:42:46 +0200

    Discarded React.js implementation

---
+++ server/app/js/map.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var map;
+
+// Request the data from the REST API
+function requestData() {
+	$.ajax({
+		url: getURL() + "/things",
+		global: false,
+		type: 'GET',
+		async: false,
+		success: function(things) {
+			drawMarkers(things);
+		}
+	});
+};
+
+// Draw the markers on the map
+function drawMarkers(things) {
+	// TODO: This is only a quick-and-dirty version. As soon as the REST API returns valid GeoJSON, this has to be changed adequately
+	var geoJsonFeatures = [];
+	things.forEach(function(thing, key) {
+		geoJsonFeatures.push({
+			type: 'Feature',
+			geometry: {
+				type: 'Point',
+				coordinates: [
+					thing.location[1], thing.location[0],
+				]
+			},
+			'properties': {
+				'title': '<h5><span class="label label-success">' + thing.description + '</span></h5>'
+			}
+		});
+	});
+
+	// Add markers to the map
+	var markers = L.mapbox.featureLayer(geoJsonFeatures, {
+		pointToLayer: function(feature, latlon) {
+			return L.circleMarker(latlon, {
+				radius: 3,
+				weight: 1,
+				opacity: 1,
+				fillOpacity: 0.8
+			});
+		}
+	}).addTo(map);
+
+	// Pan the map so that all markers are visible
+	map.fitBounds(markers.getBounds());
+};
+
+// Initialize the map
+$(document).ready(function() {
+	L.mapbox.accessToken = getMapboxAccessToken();
+	map = L.mapbox.map('livemap', 'mapbox.streets')
+		.setView([51.973387, 7.700213], 10)
+		.addControl(L.mapbox.geocoderControl('mapbox.places'));
+
+	requestData();
+});
--- server/server/config.js
+++ server/server/config.js
@@ -1,12 +1,9 @@
 var host = 'giv-gwot-va.uni-muenster.de';
 var port = 3000;
 
-var mapbox_token = '<< your Mapbox access token >>';
-
 module.exports = {
 	express_host: 'http://' + host + ':' + port,
 	express_port: port,
-	mapbox_token: mapbox_token,
 	mongodb_host: 'mongodb://' + host + '/wot-vertical-approach',
 	mqtt_host: 'mqtt://' + host + '/'
 };
