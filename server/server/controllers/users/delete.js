2c307b888e743235eb06cfddbedcafeed3fec3aa
Marius Runde <marius.runde@uni-muenster.de>
2016-05-09 16:47:48 +0200

    Updated users.delete
    To avoid greater complexity, the function makes a REST DELETE-request
    for all things associated to the user

--- server/server/controllers/users/delete.js
+++ server/server/controllers/users/delete.js
@@ -1,8 +1,15 @@
+// Load the application's configuration
+var config = require('../../config');
+var url    = config.express_host + '/api';
+
 // Required modules
+var async    = require('async');
 var mongoose = require('mongoose');
+var request  = require('request');
 
 // Required data schema
-var User = require('../../data/user');
+var Thing = require('../../data/thing');
+var User  = require('../../data/user');
 
 /**
  * @api {delete} /users/:userId DELETE - Delete a User
@@ -25,11 +32,36 @@ var User = require('../../data/user');
 exports.request = function(req, res) {
 	var id = req.params.userId;
 	
-	User.remove({ _id: id }, function(err, removed) {
+	async.waterfall([
+		// Get Things
+		function(callback) {
+			Thing.find({ userId: id }, function(err, things) {
+				callback(err, things);
+			});
+		},
+		// Delete Things with REST requests to avoid greater complexity
+		function(things, callback) {
+			async.forEachOf(things, function(thing, key, callback) {
+				request.delete({
+					url: url + '/things/' + thing._id
+					}, function(error, response, body) {
+						callback(error);
+				});
+			}, function (err) {
+				callback(err);
+			});
+		},
+		// Delete User
+		function(callback) {
+			User.remove({ _id: id }, function(err, removed) {
+				callback(err, removed);
+			});
+		}
+	], function(err, result) {
 		if (err) {
 			res.send(err);
 		} else {
-			res.json(removed);
+			res.json(result);
 		}
 	});
 }
