afbd65b15cccb5aff5486c1138b0ebf27ebd217e
Marius Runde <marius.runde@uni-muenster.de>
2016-06-13 20:41:11 +0200

    Fixed automation of the demoSensorController
    Everything except the waterbody is now created for the demonstration
    automatically

--- server/demo/demoSensorController.js
+++ server/demo/demoSensorController.js
@@ -6,6 +6,7 @@ const url  = config.express_host + '/api';
 
 // Required modules
 var async   = require('async');
+var colors  = require('colors');
 var request = require('request');
 
 // Read the arguments from the command line or set them to the default values
@@ -14,19 +15,52 @@ const thingName   = process.argv[3] || 'Demo';
 const thingLocLat = process.argv[4] || 51.964113;
 const thingLocLng = process.argv[5] || 7.624862;
 
-console.log('Starting demonstration...');
+console.log('////////////////////////////////////////////////////////////');
+console.log('');
+console.log('            STARTING DEMONSTRATION...'.cyan);
+console.log('');
+console.log('////////////////////////////////////////////////////////////');
+console.log('');
 
 async.waterfall([
-	// Create a new Thing
+	// Create a new User
 	function(callback) {
-		console.log('Creating a new Thing...');
+		console.log('  Creating a new', 'User...'.cyan);
+
+		const userJson = {
+			name: 'demoUser#' + Math.random(),
+			password: 'demo',
+			email: 'demo@example.com'
+		};
+
+		// Post the new User
+		request.post({
+			headers: {'content-type': 'application/json'},
+			url: url + '/users',
+			json: userJson
+		}, function(error, response, body) {
+			if (!error) {
+				console.log('  New User', 'created.'.green);
+			} else {
+				console.log('  New User creation', 'failed'.red);
+			}
+			console.log('');
+			console.log('------------------------------------------------------------');
+			console.log('');
+			callback(error, body._id);
+		});
+	},
+	// Create a new Thing
+	function(userId, callback) {
+		console.log('  Creating a new', 'Thing...'.cyan);
 		
 		const thingJson = {
 			name: thingName,
 			loc: {
 				coordinates: [ thingLocLat, thingLocLng ]
 			},
-			userId: '572b5f591d02fae81d017729'
+			userId: userId,
+			waterbodyId: '5752d2d7e5d703480187e0d9'
 		};
 		
 		// Post the new Thing
@@ -34,22 +68,56 @@ async.waterfall([
 			headers: {'content-type': 'application/json'},
 			url: url + '/things',
 			json: thingJson
-			}, function(error, response, body) {
-				if (!error) {
-					console.log('New Thing created.');
-				}
-				callback(error, body._id);
+		}, function(error, response, body) {
+			if (!error) {
+				console.log('  New Thing', 'created.'.green);
+			} else {
+				console.log('  New Thing creation', 'failed'.red);
+			}
+			console.log('');
+			console.log('------------------------------------------------------------');
+			console.log('');
+			callback(error, body._id);
+		});
+	},
+	// Create a new Feature
+	function(thingId, callback) {
+		console.log('  Creating a new', 'Feature...'.cyan);
+
+		const featureJson = {
+			name: 'demoFeature',
+			unit: 'foo'
+		};
+
+		// Post the new Feature
+		request.post({
+			headers: {'content-type': 'application/json'},
+			url: url + '/features',
+			json: featureJson
+		}, function(error, response, body) {
+			if (!error) {
+				console.log('  New Feature', 'created'.green);
+			} else {
+				console.log('  New Feature creation', 'failed'.red);
+			}
+			console.log('');
+			console.log('------------------------------------------------------------');
+			console.log('');
+			callback(error, thingId, body._id);
 		});
 	},
 	// Create a new Sensor
-	function(thingId, callback) {
-		console.log('Creating a new Sensor...');
+	function(thingId, featureId, callback) {
+		console.log('  Creating a new', 'Sensor...'.cyan);
 
 		const sensorJson = {
 			name: 'water gauge',
-			thingId: thingId,
 			interval: interval,
-			featureId: '572f0ad61d9ea4334b1adf72'
+			refLevel: 2,
+			warnLevel: 7,
+			riskLevel: 9,
+			thingId: thingId,
+			featureId: featureId
 		};
 
 		// Post the new Sensor
@@ -57,17 +125,22 @@ async.waterfall([
 			headers: {'content-type': 'application/json'},
 			url: url + '/sensors',
 			json: sensorJson
-			}, function(error, response, body) {
-				if (!error) {
-					console.log('New Sensor created.');
-				}
-				callback(error, body._id);
+		}, function(error, response, body) {
+			if (!error) {
+				console.log('  New Sensor', 'created.'.green);
+			} else {
+				console.log('  New Sensor creation', 'failed'.red);
+			}
+			console.log('');
+			console.log('------------------------------------------------------------');
+			console.log('');
+			callback(error, body._id);
 		});
 	},
 	// Create new Measurements in an interval
 	function(sensorId, callback) {
 		setInterval(function() {
-			console.log('Creating a new Measurement...');
+			console.log('  Creating a new', 'Measurement...'.cyan);
 			
 			// Calculate the Measurement's value as a random number between 0 and 10
 			let value = Math.random() * 10;
@@ -83,12 +156,16 @@ async.waterfall([
 				headers: {'content-type': 'application/json'},
 				url: url + '/measurements',
 				json: measurementJson
-				}, function(error, response, body) {
-					if (error) {
-						callback(error);
-					} else {
-						console.log('New Measurement created.');
-					}
+			}, function(error, response, body) {
+				if (!error) {
+					console.log('  New Measurement', 'created.'.green);
+				} else {
+					console.log('  New Measurement creation', 'failed'.red);
+					callback(error);
+				}
+				console.log('');
+				console.log('------------------------------------------------------------');
+				console.log('');
 			});
 		}, interval);
 	}
