fa7175777f39890551b2a18a95e56bb111f2420f
Marius Runde <marius.runde@uni-muenster.de>
2016-06-27 18:23:30 +0200

    Merged all API routes into single router

--- server/server/routes/features.js
+++ server/server/routes/features.js
@@ -1,32 +1,7 @@
-// Required modules
-var express = require('express');
-
-// Required controllers
-var del  = require('../controllers/features/delete');
-var get  = require('../controllers/features/get');
-var list = require('../controllers/features/list');
-var post = require('../controllers/features/post');
-
-// Set up the express router
-var router = express.Router();
-
-// --------------------------------------------------
-// DELETE
-// --------------------------------------------------
-router.delete('/features/:featureId', del.request);
-
-// --------------------------------------------------
-// GET
-// --------------------------------------------------
-
-// Get one feature
-router.get('/features/:featureId', get.request);
-// Get all features
-router.get('/features', list.request);
-
-// --------------------------------------------------
-// POST
-// --------------------------------------------------
-router.post('/features', post.request);
-
-module.exports = router;
+// Export controllers
+module.exports = {
+	del:  require('../controllers/features/delete'),
+	get:  require('../controllers/features/get'),
+	list: require('../controllers/features/list'),
+	post: require('../controllers/features/post')
+};
--- server/server/routes/measurements.js
+++ server/server/routes/measurements.js
@@ -1,41 +1,10 @@
-// Required modules
-var express = require('express');
-
-// Required controllers
-var del          		= require('../controllers/measurements/delete');
-var get          		= require('../controllers/measurements/get');
-var list        		= require('../controllers/measurements/list');
-var listSpatial  		= require('../controllers/measurements/listSpatial');
-var listTemporal 		= require('../controllers/measurements/listTemporal');
-var listSpatialTemporal = require('../controllers/measurements/listSpatialTemporal');
-var post         		= require('../controllers/measurements/post');
-
-// Set up the express router
-var router = express.Router();
-
-// --------------------------------------------------
-// DELETE
-// --------------------------------------------------
-router.delete('/measurements/:measurementId', del.request);
-
-// --------------------------------------------------
-// GET
-// --------------------------------------------------
-
-// Get one measurement
-router.get('/measurements/:measurementId', get.request);
-// Get all measurements
-router.get('/measurements', list.request);
-// Get all measurements within one bounding box
-router.get('/measurements/spatial/:bbox', listSpatial.request);
-// Get all measurements within one time frame
-router.get('/measurements/temporal/:dateFrom/:dateTo', listTemporal.request);
-// Get all measurements within one time frame and one bounding box
-router.get('/measurements/temporal/:dateFrom/:dateTo/spatial/:bbox', listSpatialTemporal.request);
-
-// --------------------------------------------------
-// POST
-// --------------------------------------------------
-router.post('/measurements', post.request);
-
-module.exports = router;
+// Export controllers
+module.exports = {
+	del:                 require('../controllers/measurements/delete'),
+	get:                 require('../controllers/measurements/get'),
+	list:                require('../controllers/measurements/list'),
+	listSpatial:         require('../controllers/measurements/listSpatial'),
+	listTemporal:        require('../controllers/measurements/listTemporal'),
+	listSpatialTemporal: require('../controllers/measurements/listSpatialTemporal'),
+	post:                require('../controllers/measurements/post')
+};
