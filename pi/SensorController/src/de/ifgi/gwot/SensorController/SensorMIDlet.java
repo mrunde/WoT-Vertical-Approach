9abc778f7ac05f94da819b881c83ec374967b5b8
ummelmann <d.ummelmann@gmail.com>
2016-07-20 22:52:51 +0200

    Added GPS-Coordinates to SensorController

--- pi/SensorController/src/de/ifgi/gwot/SensorController/SensorMIDlet.java
+++ pi/SensorController/src/de/ifgi/gwot/SensorController/SensorMIDlet.java
@@ -16,8 +16,14 @@ public class SensorMIDlet extends MIDlet{
 	private static final int TRIGGER_PIN = 20;
 	private static final int ECHO_PIN = 21;
 	
+	GPSSwitch switch1;
+	GPSCommSensor gps;
+	
 	private volatile boolean shouldRun = false;
 	private ReadSensors sensorsTask;
+	
+	private double latitude = Double.parseDouble(this.getAppProperty("latitude"));
+	private double longitude = Double.parseDouble(this.getAppProperty("longitude"));
 	
 	private MqttHandler mqttHandler;
 	
@@ -45,7 +51,9 @@ public class SensorMIDlet extends MIDlet{
 	 * Initiates the GPIO Pins and starts a thread to take measurements.
 	 */
 	@Override
-	protected void startApp() throws MIDletStateChangeException {	
+	protected void startApp() throws MIDletStateChangeException {
+		
+		
 		// init ultrasound sensor
 		try {
 			hcsr04 = new HCSR04Device(TRIGGER_PIN, ECHO_PIN);
@@ -53,6 +61,30 @@ public class SensorMIDlet extends MIDlet{
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Unable to init Pins.");
+		}
+		
+		//init gps sensor
+		switch1 = new GPSSwitch();
+		try {
+			switch1.start();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			gps = new GPSCommSensor("ttyS0");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			Position pos = gps.getPosition();
+			this.latitude = pos.getLatitude();
+			this.longitude = pos.getLongitude();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		// request configuration from server
@@ -136,7 +168,7 @@ public class SensorMIDlet extends MIDlet{
 	private void requestConfiguration() throws IOException{
 		// send post thing request
 		String thing = HttpUtil.post(REST_API_URL + "api/things/", JSONUtil.encodePostThingRequest(
-				Double.parseDouble(this.getAppProperty("latitude")), Double.parseDouble(this.getAppProperty("longitude")), this.getAppProperty("User-Id"), this.getAppProperty("token")));
+				latitude, longitude, this.getAppProperty("User-Id"), this.getAppProperty("token")));
 		// send get features request
 		String allFeatures = HttpUtil.get(REST_API_URL + "api/features/");
 		
